7b9a1888e42cb1c869b7ae752ed83eb7c7674103
aziz mobarak <mobarakaziz9@gmail.com>
2021-02-22 14:14:37 +0100

    allbpost setup

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ var cat4 = [parseInt(process.env.CAT4)];
 var cat5 = [parseInt(process.env.CAT5)];
 var cat6 = [parseInt(process.env.CAT6)];
 var cat7 = [parseInt(process.env.CAT7)];
-var cat8 = [parseInt(process.env.CAT8)];
-var cat9 = [parseInt(process.env.CAT9)];
-var cat10 = [parseInt(process.env.CAT10)];
-var cat11 = [parseInt(process.env.CAT11)];
-var cat12 = [parseInt(process.env.CAT12)];
-var cat13 = [parseInt(process.env.CAT13)];
-var cat14 = [parseInt(process.env.CAT14)];
-var cat15 = [parseInt(process.env.CAT15)];
-var cat16 = [parseInt(process.env.CAT16)];
-var cat17 = [parseInt(process.env.CAT17)];
-var cat18 = [parseInt(process.env.CAT18)];
-var cat19 = [parseInt(process.env.CAT19)];
-var cat20 = [parseInt(process.env.CAT20)];
+// var cat8 = [parseInt(process.env.CAT8)];
+// var cat9 = [parseInt(process.env.CAT9)];
+// var cat10 = [parseInt(process.env.CAT10)];
+// var cat11 = [parseInt(process.env.CAT11)];
+// var cat12 = [parseInt(process.env.CAT12)];
+// var cat13 = [parseInt(process.env.CAT13)];
+// var cat14 = [parseInt(process.env.CAT14)];
+// var cat15 = [parseInt(process.env.CAT15)];
+// var cat16 = [parseInt(process.env.CAT16)];
+// var cat17 = [parseInt(process.env.CAT17)];
+// var cat18 = [parseInt(process.env.CAT18)];
+// var cat19 = [parseInt(process.env.CAT19)];
+// var cat20 = [parseInt(process.env.CAT20)];
 
 var tags = process.env.TAGS
 var lang = process.env.LANG.toString().substring(0, 2)
@@ -43,19 +43,19 @@ var keys4 = process.env.KEY4;
 var keys5 = process.env.KEY5;
 var keys6 = process.env.KEY6;
 var keys7 = process.env.KEY7;
-var keys8 = process.env.KEY8;
-var keys9 = process.env.KEY9;
-var keys10 = process.env.KEY10;
-var keys11 = process.env.KEY11;
-var keys12 = process.env.KEY12;
-var keys13 = process.env.KEY13;
-var keys14 = process.env.KEY14;
-var keys15 = process.env.KEY15;
-var keys16 = process.env.KEY16;
-var keys17 = process.env.KEY17;
-var keys18 = process.env.KEY18;
-var keys19 = process.env.KEY19;
-var keys20 = process.env.KEY20;
+// var keys8 = process.env.KEY8;
+// var keys9 = process.env.KEY9;
+// var keys10 = process.env.KEY10;
+// var keys11 = process.env.KEY11;
+// var keys12 = process.env.KEY12;
+// var keys13 = process.env.KEY13;
+// var keys14 = process.env.KEY14;
+// var keys15 = process.env.KEY15;
+// var keys16 = process.env.KEY16;
+// var keys17 = process.env.KEY17;
+// var keys18 = process.env.KEY18;
+// var keys19 = process.env.KEY19;
+// var keys20 = process.env.KEY20;
 
 
 // format keywords
@@ -78,19 +78,19 @@ var All_Keys4 = FormatKeys(keys4);
 var All_Keys5 = FormatKeys(keys5);
 var All_Keys6 = FormatKeys(keys6);
 var All_Keys7 = FormatKeys(keys7);
-var All_Keys8 = FormatKeys(keys8);
-var All_Keys9 = FormatKeys(keys9);
-var All_Keys10 = FormatKeys(keys10);
-var All_Keys11 = FormatKeys(keys11);
-var All_Keys12 = FormatKeys(keys12);
-var All_Keys13 = FormatKeys(keys13);
-var All_Keys14 = FormatKeys(keys14);
-var All_Keys15 = FormatKeys(keys15);
-var All_Keys16 = FormatKeys(keys16);
-var All_Keys17 = FormatKeys(keys17);
-var All_Keys18 = FormatKeys(keys18);
-var All_Keys19 = FormatKeys(keys19);
-var All_Keys20 = FormatKeys(keys20);
+// var All_Keys8 = FormatKeys(keys8);
+// var All_Keys9 = FormatKeys(keys9);
+// var All_Keys10 = FormatKeys(keys10);
+// var All_Keys11 = FormatKeys(keys11);
+// var All_Keys12 = FormatKeys(keys12);
+// var All_Keys13 = FormatKeys(keys13);
+// var All_Keys14 = FormatKeys(keys14);
+// var All_Keys15 = FormatKeys(keys15);
+// var All_Keys16 = FormatKeys(keys16);
+// var All_Keys17 = FormatKeys(keys17);
+// var All_Keys18 = FormatKeys(keys18);
+// var All_Keys19 = FormatKeys(keys19);
+// var All_Keys20 = FormatKeys(keys20);
 }catch(err){
      console.log(err);
  }
@@ -164,120 +164,120 @@ cron.schedule('30 14 */1 * *', () => {
 });
 
 
-// cat 8
- var num7 = 0;
-cron.schedule('35 15 */2 * *', () => {
-     GetArticles(All_Keys8[num7],cat8,[1,2], lang);
-     num7++;
-     if(num7==All_Keys8.length){num7 = 0}    
-});
+// // cat 8
+//  var num7 = 0;
+// cron.schedule('35 15 */2 * *', () => {
+//      GetArticles(All_Keys8[num7],cat8,[1,2], lang);
+//      num7++;
+//      if(num7==All_Keys8.length){num7 = 0}    
+// });
 
 
-// cat 9
- var num8 = 0;
-cron.schedule('40 16 */2 * *', () => {
-     GetArticles(All_Keys9[num8],cat9,  [1,2], lang);
-     num8++;
-     if(num8==All_Keys9.length){num8 = 0}    
-});
+// // cat 9
+//  var num8 = 0;
+// cron.schedule('40 16 */2 * *', () => {
+//      GetArticles(All_Keys9[num8],cat9,  [1,2], lang);
+//      num8++;
+//      if(num8==All_Keys9.length){num8 = 0}    
+// });
 
 
-// cat 10
- var num9 = 0;
-cron.schedule('45 17 */2 * *', () => {
-   GetArticles(All_Keys10[num9],cat10,  [1,2], lang);
-     num9++;
-     if(num9==All_Keys10.length){num9 = 0}    
-});
+// // cat 10
+//  var num9 = 0;
+// cron.schedule('45 17 */2 * *', () => {
+//    GetArticles(All_Keys10[num9],cat10,  [1,2], lang);
+//      num9++;
+//      if(num9==All_Keys10.length){num9 = 0}    
+// });
 
 
-// cat 11
- var num10 = 0;
-cron.schedule('50 18 */2 * *', () => {
-     GetArticles(All_Keys11[num10],cat11,  [1,2], lang);
-     num10++;
-     if(num10==All_Keys11.length){num10 = 0}    
-});
+// // cat 11
+//  var num10 = 0;
+// cron.schedule('50 18 */2 * *', () => {
+//      GetArticles(All_Keys11[num10],cat11,  [1,2], lang);
+//      num10++;
+//      if(num10==All_Keys11.length){num10 = 0}    
+// });
 
-// cat 12
-       var num11 = 0;
-cron.schedule('55 19 */2 * *', () => {
-        GetArticles(All_Keys12[num11],cat12,  [1,2], lang);
-     num11++;
-     if(num11==All_Keys12.length){num11 = 0}    
-});
-
-
-// cat 13
-var num12 = 0;
-cron.schedule('55 20 */2 * *', () => {
-      GetArticles(All_Keys13[num12],cat13,[1,2],lang);
-     num12++;
-     if(num12==All_Keys13.length){num12 = 0}    
-});
+// // cat 12
+//        var num11 = 0;
+// cron.schedule('55 19 */2 * *', () => {
+//         GetArticles(All_Keys12[num11],cat12,  [1,2], lang);
+//      num11++;
+//      if(num11==All_Keys12.length){num11 = 0}    
+// });
 
 
-// cat 14
-var num13 = 0;
-cron.schedule('55 21 */2 * *', () => {
-      GetArticles(All_Keys14[num13],cat14,[1,2], lang);
-     num13++;
-     if(num13==All_Keys14.length){num13 = 0}    
-});
+// // cat 13
+// var num12 = 0;
+// cron.schedule('55 20 */2 * *', () => {
+//       GetArticles(All_Keys13[num12],cat13,[1,2],lang);
+//      num12++;
+//      if(num12==All_Keys13.length){num12 = 0}    
+// });
 
 
-// cat 15
-var num14 = 0;
-cron.schedule('55 22 */2 * *', () => {
-      GetArticles(All_Keys15[num14],cat15,[1,2], lang);
-     num14++;
-     if(num14==All_Keys15.length){num14 = 0}    
-});
+// // cat 14
+// var num13 = 0;
+// cron.schedule('55 21 */2 * *', () => {
+//       GetArticles(All_Keys14[num13],cat14,[1,2], lang);
+//      num13++;
+//      if(num13==All_Keys14.length){num13 = 0}    
+// });
 
 
-// cat 16
-var num15 = 0;
-cron.schedule('55 23 */2 * *', () => {
-      GetArticles(All_Keys16[num15],cat16,  [1,2], lang);
-     num15++;
-     if(num15==All_Keys16.length){num15 = 0}    
-});
+// // cat 15
+// var num14 = 0;
+// cron.schedule('55 22 */2 * *', () => {
+//       GetArticles(All_Keys15[num14],cat15,[1,2], lang);
+//      num14++;
+//      if(num14==All_Keys15.length){num14 = 0}    
+// });
 
 
-// cat 17
-var num16 = 0;
-cron.schedule('55 00 */2 * *', () => {
-      GetArticles(All_Keys17[num16],cat17,  [1,2], lang);
-     num16++;
-     if(num16==All_Keys17.length){num16 = 0}    
-});
+// // cat 16
+// var num15 = 0;
+// cron.schedule('55 23 */2 * *', () => {
+//       GetArticles(All_Keys16[num15],cat16,  [1,2], lang);
+//      num15++;
+//      if(num15==All_Keys16.length){num15 = 0}    
+// });
 
 
-// cat 18
-var num17 = 0;
-cron.schedule('55 1 */2 * *', () => {
-      GetArticles(All_Keys18[num17],cat18,  [1,2], lang);
-     num17++;
-     if(num17==All_Keys18.length){num17 = 0}    
-});
+// // cat 17
+// var num16 = 0;
+// cron.schedule('55 00 */2 * *', () => {
+//       GetArticles(All_Keys17[num16],cat17,  [1,2], lang);
+//      num16++;
+//      if(num16==All_Keys17.length){num16 = 0}    
+// });
 
 
-// cat 19
-var num18 = 0;
-cron.schedule('55 2 */2 * *', () => {
-      GetArticles(All_Keys19[num18],cat19,  [1,2], lang);
-     num18++;
-     if(num18==All_Keys19.length){num18 = 0}    
-});
+// // cat 18
+// var num17 = 0;
+// cron.schedule('55 1 */2 * *', () => {
+//       GetArticles(All_Keys18[num17],cat18,  [1,2], lang);
+//      num17++;
+//      if(num17==All_Keys18.length){num17 = 0}    
+// });
 
 
-// cat 20
-var num19 = 0;
-cron.schedule('55 3 */2 * *', () => {
-      GetArticles(All_Keys20[num19],cat20,  [1,2], lang);
-     num19++;
-     if(num19==All_Keys20.length){num19 = 0}    
-});
+// // cat 19
+// var num18 = 0;
+// cron.schedule('55 2 */2 * *', () => {
+//       GetArticles(All_Keys19[num18],cat19,  [1,2], lang);
+//      num18++;
+//      if(num18==All_Keys19.length){num18 = 0}    
+// });
 
 
-app.listen(2010);
+// // cat 20
+// var num19 = 0;
+// cron.schedule('55 3 */2 * *', () => {
+//       GetArticles(All_Keys20[num19],cat20,  [1,2], lang);
+//      num19++;
+//      if(num19==All_Keys20.length){num19 = 0}    
+// });
+
+
+app.listen(2020);
